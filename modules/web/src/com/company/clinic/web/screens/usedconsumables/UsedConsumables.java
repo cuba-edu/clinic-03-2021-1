410b4e255e241e3ff9bccbb48638408df431d89a
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-03-25 14:10:18 +0300

    Added JS Component - not finished yet

--- modules/web/src/com/company/clinic/web/screens/usedconsumables/UsedConsumables.java
+++ modules/web/src/com/company/clinic/web/screens/usedconsumables/UsedConsumables.java
@@ -7,12 +7,17 @@ import com.haulmont.cuba.core.global.CommitContext;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.LoadContext;
 import com.haulmont.cuba.core.global.View;
+import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.VBoxLayout;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.web.widgets.CubaVerticalActionsLayout;
 import com.haulmont.reports.app.service.ReportService;
 import com.haulmont.reports.entity.Report;
 import com.haulmont.reports.gui.ReportGuiManager;
+import com.vaadin.ui.AbstractOrderedLayout;
+import com.vaadin.ui.Slider;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -34,6 +39,21 @@ public class UsedConsumables extends Screen {
 
     @Inject
     private ReportGuiManager reportGuiManager;
+    @Inject
+    private Notifications notifications;
+    @Inject
+    private VBoxLayout sliderBox;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        Slider slider = new Slider("");
+        slider.setMin(0);
+        slider.setMax(100);
+        slider.addValueChangeListener(event1 -> {
+            notifications.create().withCaption(event1.getValue().toString()).show();
+        });
+        sliderBox.unwrap(CubaVerticalActionsLayout.class).addComponent(slider);
+    }
 
 /*
     @Subscribe
@@ -45,6 +65,8 @@ public class UsedConsumables extends Screen {
         consumablesDc.setItems(consumables);
     }
 */
+
+
 
     private void loadByService() {
         List<Consumable> consumables = consumablesService.getUsedConsumables();
--- modules/web/src/com/company/clinic/web/screens/visit/VisitEdit.java
+++ modules/web/src/com/company/clinic/web/screens/visit/VisitEdit.java
@@ -2,15 +2,18 @@ package com.company.clinic.web.screens.visit;
 
 import com.company.clinic.entity.Consumable;
 import com.company.clinic.service.VisitService;
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.Dialogs;
 import com.haulmont.cuba.gui.model.CollectionChangeType;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.clinic.entity.Visit;
+import com.haulmont.cuba.web.gui.components.JavaScriptComponent;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 @UiController("clinic_Visit.edit")
 @UiDescriptor("visit-edit.xml")
@@ -22,6 +25,18 @@ public class VisitEdit extends StandardEditor<Visit> {
     private VisitService visitService;
     @Inject
     private Logger log;
+    @Inject
+    private JavaScriptComponent descrJs;
+
+    class QuillState {
+        public Map<String, Object> options;
+    }
+
+    private void initJsEditor() {
+        QuillState state = new QuillState();
+        state.options = ParamsMap.of("theme", "snow");
+        descrJs.setState(state);
+    }
 
     @Subscribe(id = "visitDc", target = Target.DATA_CONTAINER)
     public void onVisitDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Visit> event) {
